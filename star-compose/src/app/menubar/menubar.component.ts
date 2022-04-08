e5f4c46a2a14a07245feadeff697223304b0f576
ashleyvn <ashleyvn@vt.edu>
2022-04-08 17:09:46 -0400

    hide boxes

--- star-compose/src/app/menubar/menubar.component.ts
+++ star-compose/src/app/menubar/menubar.component.ts
@@ -15,6 +15,33 @@ export class MenubarComponent implements OnInit {
 
   runConductor() {
     this.buttonPressed.emit(true);
+    this.navbarOpened = false
+
+    let icons = [];
+    icons.push(document.getElementsByClassName("outside"))
+    icons.push(document.getElementsByClassName("drag-icon"))
+    icons.push(document.getElementsByClassName("rotate-icon"))
+    icons.push(document.getElementsByClassName("resize-grip"))
+    if (icons[0] != null) {
+      for (var i = 0; i < icons.length; i++) {
+        for (var j = 0; j < icons[i].length; j++) {
+          if (i == 0) {
+            var str = icons[i][j].getAttribute("style")
+            if ( str == null) {
+              str = ""
+            }
+            icons[i][j].setAttribute("style", str + "border: none;")
+          }
+          else {
+            var str = icons[i][j].getAttribute("style")
+            if ( str == null) {
+              str = ""
+            }
+            icons[i][j].setAttribute("style", str + "visibility: hidden;")
+          }
+        }
+      }
+    }
   }
 
   //Add constellation to sky on click
